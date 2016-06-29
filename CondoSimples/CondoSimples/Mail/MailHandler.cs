dbba1c32abca7644c3d700c9af3120c3207221fe
elis-oliveira <elis.oliveira@fatec.sp.gov.br>
2016-06-29 19:14:52 -0300

    contato e ajustes iniciais da view home

--- CondoSimples/CondoSimples/Mail/MailHandler.cs
+++ CondoSimples/CondoSimples/Mail/MailHandler.cs
@@ -33,5 +33,29 @@ namespace CondoSimples.Mail
                 smtp.Send(message);
             }
         }
+
+        public static void SendMailToUs(string body, string mailFrom, string subject)
+        {
+            var message = new MailMessage();
+            message.To.Add(new MailAddress("condosimples@outlook.com.br"));  // replace with valid value 
+            message.From = new MailAddress(mailFrom);  // replace with valid value
+            message.Subject = subject;
+            message.Body = body;
+            message.IsBodyHtml = true;
+
+            using (var smtp = new SmtpClient())
+            {
+                var credential = new NetworkCredential
+                {
+                    UserName = "condosimples@outlook.com.br",  // replace with valid value
+                    Password = "atireiopaunogato666"  // replace with valid value
+                };
+                smtp.Credentials = credential;
+                smtp.Host = "smtp-mail.outlook.com";
+                smtp.Port = 587;
+                smtp.EnableSsl = true;
+                smtp.Send(message);
+            }
+        }
     }
 }
